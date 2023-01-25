0082ddd6c45dd3bea5b4c4fda73d5eb039ae1f8e
TessaEC <118077000+TessaEC@users.noreply.github.com>
2023-01-24 23:20:57 -0600

    finished

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -17,33 +17,35 @@ submitBtn.addEventListener('click', function() {
             getCityWeather(cityName);
         }
         var dropCity = JSON.parse(localStorage.getItem('searched-city')) || [];
-            if (!dropCity.matches(cityName)) {
+            if (!dropCity.includes(cityName)) {
                 dropCity.push(cityName);
-            };
+            }
 
 localStorage.setItem('searched-city', JSON.stringify(dropCity));
 
-console.log(cityName)   
-
 displayHistory();
+
 });
 
 // displays searched city as buttons from local storage
 function displayHistory() {
-    var history = JSON.parse(localStorage.getItem('searched-city')) || [];    
-
+    var history = JSON.parse(localStorage.getItem('searched-city')) || []; 
+  
 $.each(history, function(index, value) {
     var button = document.createElement('button');
-    button.innerHTML = value;
-
+    button.innerHTML = value;        
+   
     button.addEventListener('click', function(){
     getCityWeather(value);
 });
 
     document.getElementById("history-div").appendChild(button);
- 
 });
 }
+
+$('#submit').submit(function(){
+    $('#submit', this).attr('disabled', 'disabled');
+ })
 // Ajax call for current weather info using Open Weather API
 function getCityWeather(cityName) {
     var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIkey}&units=imperial`;
@@ -55,8 +57,6 @@ function getCityWeather(cityName) {
         console.log(res);
 
         var currentWeather = res.list[0];
-        // var fiveDayWeather = res.list[2];
-        
 
         showCurrent(currentWeather);
         showFiveDay(res)
@@ -65,8 +65,8 @@ function getCityWeather(cityName) {
 // Current weather card to display info using template literals
 function showCurrent(data) {
     var cardCurrent =`
-    <div class="row">
-    <div class="card">
+    <div class="column">
+        <div class="card">
         <div class="card-body">
             <h2>Current Weather for: ${cityName}</h2>
           <p>Temp: ${data.main.temp} °F</p>
@@ -76,12 +76,13 @@ function showCurrent(data) {
       </div>
     </div>`
     $("#showCurrent").html(cardCurrent)
+    
 };
 // 5 day forecast weather card to display info using template literals
 function showFiveDay(data) {
  var cardFive = ""
     // loop card for each day of 5-day forecast
-    for (var i = 1; i < data.list.length; i ++) {
+    for (var i = 0; i < data.list.length; i ++) {
         if (i % 8 === 0) {
             var currentDay  = data.list[i] 
         cardFive += `
@@ -104,76 +105,3 @@ function showFiveDay(data) {
 };
 
 displayHistory();
-
-
-// Issues:
-
-
-// format cards, use day.js to format time, format buttons
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-// submitBtn.addEventListener('click', function() {
-//     var cityName = cityInput.value
-
-//     if(cityName === "") {
-//         return
-//     } else {
-//         getCityWeather(cityName);
-//     }
-//     var dropCity = JSON.parse(localStorage.getItem('searched-city')) || [];
-
-//     dropCity.push(cityName);
-//     localStorage.setItem('searched-city', JSON.stringify(dropCity));
-
-//     console.log(cityName)
-// });
-
-
-// function getCoords(search) {
-//     var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIkey}&units=imperial`;
-  
-//     fetch(url)
-//       .then(function (res) {
-//         return res.json();
-//       })
-//       .then(function (data) {
-//         if (!data[0]) {
-//           alert('Location not found');
-//         } else {
-//           appendToHistory(search);
-//           fetchWeather(data[0]);
-//         }
-//       })
-//       .catch(function (err) {
-//         console.error(err);
-//       });
-//   }
-
-
-
-   
-// function weatherEvent() {
-//     var enterBtn = document.querySelector('#enter');
-    
-//     enterBtn.addEventListener('keypress', function(event) {
-//         if (event.key === "Enter") {
-//             event.preventDefault();
-
-//             document.getElementById('submitBtn').click();
-//         }
-//     });
-// }
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # weather-dash
-Bootcamp Challenge #6 - Dashboard to retrieve weather data for cities.
+Bootcamp Challenge #6 - Dashboard to retrieve weather data using Open Weather API.
+
+## Description
+The user will happen upon a friendly and playful interface where they will be able to perform a search for their desired city. Once the user has searched for their city, they will see current and a future 5-day weather forecast.
+
+## Usage
+This weather dashboard uses Open Weather's API to grab the selected weather information and display it onto formatted cards. Once the user clicks the "Rain or Shine?" button to submit their search input, the weather will be displayed. They will be able to see the temperature, humidity and wind speed for that city. On top of being able to search for a city, all input is saved and displayed under search history as a button for the user to refer back to at a later time.
+
+## Visuals:
+
+### Javascript Functionality
+Javascript has a combination of creating buttons, using an ajax call to retrieve Open Weather's information using an API, and template literals inserted into HTML cards to retrieve specific information to be displayed onto the formatted cards.
+
+
+![image](https://user-images.githubusercontent.com/118077000/214485391-c46821ea-6365-412c-a095-87c188db3ca6.png)
+
+![image](https://user-images.githubusercontent.com/118077000/214485544-e5c9a5ad-ef4a-42e1-936a-8fb0f4361e28.png)
+
+![image](https://user-images.githubusercontent.com/118077000/214485682-02d7e77b-0bc7-4ba7-a653-e9edfab5bff3.png)
+
+![image](https://user-images.githubusercontent.com/118077000/214485778-db148119-603d-47f0-95b5-5edcfdf30268.png)
+
+
+### Building CSS
+CSS was accompanied by Bootstrap styling on this project.
+
+#### Website URL: 
+
